f0d77b96d80eb790a1af2ace235146cc41526bba
Massimo Aufiero <aufieroma12@gmail.com>
2023-04-24 21:04:09 -0700

    Load toxicity metric in init

--- objective.py
+++ objective.py
@@ -1,12 +1,10 @@
 """Classes for objective functions."""
 from abc import ABC, abstractmethod
-from typing import List, Union, cast
+from typing import List, cast
 
 import evaluate
 from nltk import sent_tokenize
 import pandas as pd
-
-base_toxicity_metric = evaluate.load("toxicity")
 
 
 # TODO: consider having the objective wrap the model as well
@@ -22,22 +20,23 @@ class Objective(ABC):
 class ToxicityObjective(Objective):
     """Toxicity score objective function."""
 
+    def __init__(self):
+        """Initialize the underlying evaluation model."""
+        self.base_toxicity_metric = evaluate.load("toxicity")
+
     def __call__(self, texts: List[str]) -> float:
         """Compute the objective value for a given text batch."""
-        return toxicity_classifier(texts).mean()
-
-
-def toxicity_classifier(texts: Union[List[str], pd.Series]) -> pd.Series:
-    """Return the toxicity scores for each text."""
-    # Empirically, splitting each text into separate sentences and aggregating over
-    # their scores seems to work better than classifying the full text.
-    texts_series = pd.Series(texts) if isinstance(texts, list) else texts
-    sentences = cast(pd.Series, texts_series.apply(_safe_sent_tokenize)).explode()
-    sentence_scores = pd.Series(
-        base_toxicity_metric.compute(predictions=sentences.to_list())["toxicity"],
-        index=sentences.index,
-    )
-    return sentence_scores.groupby(sentence_scores.index).max()
+        # Empirically, splitting each text into separate sentences and aggregating over
+        # their scores seems to work better than classifying the full text.
+        texts_series = pd.Series(texts)
+        sentences = cast(pd.Series, texts_series.apply(_safe_sent_tokenize)).explode()
+        sentence_scores = pd.Series(
+            self.base_toxicity_metric.compute(
+                predictions=sentences.to_list()
+            )["toxicity"],
+            index=sentences.index,
+        )
+        return sentence_scores.groupby(sentence_scores.index).max().mean()
 
 
 def _safe_sent_tokenize(text: str) -> List[str]:
