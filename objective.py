81dde42a6e400d061dd4f1d61551fa4db4d9874e
Massimo Aufiero <aufieroma12@gmail.com>
2023-04-24 21:13:48 -0700

    Add sentiment objective

--- objective.py
+++ objective.py
@@ -5,6 +5,7 @@ from typing import List, cast
 import evaluate
 from nltk import sent_tokenize
 import pandas as pd
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 
 
 # TODO: consider having the objective wrap the model as well
@@ -43,3 +44,31 @@ def _safe_sent_tokenize(text: str) -> List[str]:
     """Return a list of sentences for a given text."""
     sentences = sent_tokenize(text)
     return sentences or [""]
+
+
+_DEFAULT_SENTIMENT_MODEL_URI = "cardiffnlp/twitter-roberta-base-sentiment"
+_DEFAULT_POSITIVE_INDEX = 2
+
+
+class SentimentObjective(Objective):
+    """Objective measuring the sentiment of a text."""
+
+    def __init__(
+        self,
+        sentiment_model_uri: str = _DEFAULT_SENTIMENT_MODEL_URI,
+        positive_index: int = _DEFAULT_POSITIVE_INDEX,
+    ):
+        """Initialize the underlying evaluation model."""
+        self.tokenizer = AutoTokenizer.from_pretrained(sentiment_model_uri)
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            sentiment_model_uri
+        ).eval()
+        self.positive_index = positive_index
+
+    def __call__(self, texts: List[str]) -> float:
+        """Compute the objective value for a given text batch."""
+        inputs = self.tokenizer(texts, return_tensors="pt")
+        outputs = self.model(**inputs)
+        logits = outputs.logits
+        probs = logits.softmax(dim=1)
+        return probs[:, self.positive_index].mean().item()
